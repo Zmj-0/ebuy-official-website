25ff62dcb16870c18ebad903fed9c31187c6ef1d
ldw <106669840+qqqqqqw2@users.noreply.github.com>
2025-11-24 14:52:17 +0800

    update(删除了Google Play下载功能)

--- src/components/download/banner/index.tsx
+++ src/components/download/banner/index.tsx
@@ -5,8 +5,6 @@ import phone from './images/phone1.png'
 import Image from "next/image";
 import app_1 from "./images/app-1-m.png"
 import ecode from "./images/ecode-m.png"
-import googlePlay from './images/googlePlay.png'
-import googlePlay_m from './images/googlePlay-m.png'
 import { useI18n, useInView, useWindowSize ,useResponsive} from "@/hooks";
 // import { useRouter } from "next/router";
 import { useEffect, useState,useRef } from "react";
@@ -16,7 +14,6 @@ export const Banner = () => {
   const t=useI18n('download')
   const imgElement=useRef(null)
   const phoneImg=useRef(null)
-  const googlePlay1 = useRef(null)
   // 切换手机大图
   const { md } = useResponsive();
 
@@ -36,22 +33,6 @@ export const Banner = () => {
         };
     }
    }, [md]);
-     //点击跳转Google Play下载
-     useEffect(() => {
-      const googleApp = (element: any) => {
-          const handleClick = () => {
-              window.location.href = "https://play.google.com/store/apps/details?id=com.easybuy.administrator.easybuycustomer&hl=en";
-          };
-          element.addEventListener("click", handleClick);
-          return () => element.removeEventListener("click", handleClick);
-      };
-      if (md) {
-          const cleanup1 = googleApp(googlePlay1.current);
-          return () => {
-              cleanup1();
-          };
-      }
-  }, [md]);
 
   const nowWindowSize:any=useWindowSize()
   //  手机图逐渐放大的效果
@@ -108,7 +89,6 @@ export const Banner = () => {
           {/* 图片 下载方式 */}
           <div className={classNames(styles.ecode_content)}>
             <Image ref={imgElement} src={app_1} alt="" className={classNames('w-[214px] h-[65px]')}></Image>
-            <Image ref={googlePlay1} src={!md?googlePlay:googlePlay_m} alt=""  className={classNames('w-[237px] ml-[24px]')}></Image>
             <Image src={ecode} alt=""  className={classNames('w-[120px] ml-[24px]')}></Image>
           </div>
           
--- src/components/download/download_app/index.tsx
+++ src/components/download/download_app/index.tsx
@@ -4,7 +4,6 @@ import phone2 from "./images/phone2.png";
 import phone1_m from "./images/phone1-m.png";
 import phone2_m from "./images/phone2-m.png";
 import ecode from "./images/ecode.png"
-import googlePlay from "./images/googlePlay.png"
 import store from "./images/apple_store.png"
 import classNames from "classnames";
 import styles from "./styles.module.scss";
@@ -12,7 +11,6 @@ import { useI18n, useInView, windowSizeRange, useResponsive } from "@/hooks";
 import { useState, useEffect, useRef } from "react";
 import useScrollAnimation from '@/hooks/useScrollAnimation';
 import useScrollDirection from '@/hooks/useScrollDirection';
-import { log } from "console";
 
 export const DownloadApp = () => {
     const { md } = useResponsive()
@@ -21,8 +19,6 @@ export const DownloadApp = () => {
 
     const codeElement1 = useRef(null);
     const codeElement2 = useRef(null);
-    const googlePlay1 = useRef(null)
-    const googlePlay2 = useRef(null)
     //点击跳转苹果商城下载
     useEffect(() => {
         const downloadApp = (element: any) => {
@@ -33,6 +29,7 @@ export const DownloadApp = () => {
             return () => element.removeEventListener("click", handleClick);
         };
         if (md) {
+            //绑定click事件
             const cleanup1 = downloadApp(codeElement1.current);
             const cleanup2 = downloadApp(codeElement2.current);
             return () => {
@@ -41,25 +38,6 @@ export const DownloadApp = () => {
             };
         }
     }, [md]);
-    //点击跳转Google Play下载
-    useEffect(() => {
-        const googleApp = (element: any) => {
-            const handleClick = () => {
-                window.location.href = "https://play.google.com/store/apps/details?id=com.easybuy.administrator.easybuycustomer&hl=en";
-            };
-            element.addEventListener("click", handleClick);
-            return () => element.removeEventListener("click", handleClick);
-        };
-        if (md) {
-            const cleanup1 = googleApp(googlePlay1.current);
-            const cleanup2 = googleApp(googlePlay2.current);
-            return () => {
-                cleanup1();
-                cleanup2();
-            };
-        }
-    }, [md]);
-
     // 判断窗口大小渲染相应图片内容
     const nowPhoto = (pic1: any, pic2: any) => {
         if (!md) {
@@ -118,7 +96,6 @@ export const DownloadApp = () => {
                     {/* 二维码 */}
                     <div className={classNames(styles.ecode_content)}>
                         <Image ref={codeElement1} src={store} alt="" className={classNames(styles.ecode_store)}></Image>
-                        <Image ref={googlePlay1} src={googlePlay} alt="" className={classNames(styles.googlePlay)}></Image>
                         <Image src={ecode} alt="" className={classNames(styles.ecode)}></Image>
                     </div>
                 </div>
@@ -134,7 +111,6 @@ export const DownloadApp = () => {
                     {/* 二维码 */}
                     <div className={classNames(styles.ecode_content)}>
                         <Image ref={codeElement2} src={store} alt="" className={classNames(styles.ecode_store)}></Image>
-                        <Image ref={googlePlay2} src={googlePlay} alt="" className={classNames(styles.googlePlay)}></Image>
                         <Image src={ecode} alt="" className={classNames(styles.ecode)}></Image>
                     </div>
                 </div>
